16b03795d5d4465b95692001ba806b3d971b711f
Indra Zulfi <me@indrazm.com>
2023-08-14 15:17:05 +0700

    chore: Setup ShadcnUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div>Hey!</div>;
+  return <div>Hey</div>;
 }
--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { RecoilRoot } from 'recoil';
+import { Toaster } from '../ui/toaster';
 
 export const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <RecoilRoot>{children}</RecoilRoot>;
+  return (
+    <>
+      <RecoilRoot>{children}</RecoilRoot>
+      <Toaster />
+    </>
+  );
 };
